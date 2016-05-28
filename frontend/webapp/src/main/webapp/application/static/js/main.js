8f24f07f703abc3313e7407c1c8cad054c092fea
David Eriksson <davideriksson@swedenmail.com>
2016-05-28 18:37:36 +0200

    jsLint

--- frontend/webapp/src/main/webapp/application/static/js/main.js
+++ frontend/webapp/src/main/webapp/application/static/js/main.js
@@ -1,14 +1,25 @@
-'use strict';
 
-var coords;
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+$(document).ready(function () {
+    'use strict';
+    $(".fancybox")
+        .attr('rel', 'gallery')
+        .fancybox({
+            maxWidth: 800,
+            maxHeight: 400,
+            fitToView: false,
+            width: '70%',
+            height: '70%',
+            autoSize: false,
+            padding: 0,
+            margin: [20, 60, 20, 60],
+            afterClose: function () {
+                location.reload();
+                return;
+            }
+        });
+
+    var closePopup = $("#closePopup").text();
+    if (closePopup !== "") {
+        parent.jQuery.fancybox.close();
     }
-}
-function showPosition(position) {
-    coords = position.coords;
-    console.log("Latitude: " + coords.latitude + ", Longitude: " + coords.longitude);
-}
-
-//getLocation();
+});
