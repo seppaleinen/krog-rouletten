2a0d472e7db3b430238202ad18bacfca931c41c9
David Eriksson <davideriksson@swedenmail.com>
2016-05-28 20:30:24 +0200

    init

--- frontend/webapp/src/main/webapp/tests/test_selenium.py
+++ frontend/webapp/src/main/webapp/tests/test_selenium.py
@@ -1,7 +1,8 @@
-import urllib2
+import urllib2, os
 from flask import Flask
 from flask_testing import LiveServerTestCase
 from application import app
+from selenium import webdriver
 
 
 class MyTest(LiveServerTestCase):
@@ -9,9 +10,22 @@ class MyTest(LiveServerTestCase):
     def create_app(self):
         app.config['TESTING'] = True
         # Default port is 5000
-        app.config['LIVESERVER_PORT'] = 8943
+        app.config['LIVESERVER_PORT'] = 8944
         return app
 
     def test_server_is_up_and_running(self):
         response = urllib2.urlopen(self.get_server_url())
         self.assertEqual(response.code, 200)
+
+    def test_(self):
+        binary = os.getenv('phantomjs.binary')
+        driver = webdriver.PhantomJS(executable_path=binary)
+        driver.set_window_size(1120, 550)
+        driver.get("https://duckduckgo.com/")
+        driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+        driver.find_element_by_id("search_button_homepage").click()
+        print driver.current_url
+        driver.quit()
+
+
+
