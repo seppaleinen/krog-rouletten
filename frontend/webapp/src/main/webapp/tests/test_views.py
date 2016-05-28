c1858a13af129670c58c06967b330a68becc9b4f
David Eriksson <davideriksson@swedenmail.com>
2016-05-28 23:35:22 +0200

    Assertions

--- frontend/webapp/src/main/webapp/tests/test_views.py
+++ frontend/webapp/src/main/webapp/tests/test_views.py
@@ -21,21 +21,23 @@ class HomeUnitTests(unittest.TestCase):
     def test_home_get(self, mocked):
         os.environ['BACKEND_URL'] = 'http://localhost:10080'
         result = self.app.get(self.url)
-        assert STATUS_200 in result.status
-        assert 'Krogrouletten' in result.data
+        #self.assertEquals(result.json, dict(success=True))
+        self.assertEquals(STATUS_200, result.status)
+        self.assertTrue('class="logo shadow">Krogrouletten</div>' in result.data)
         mocked.get.assert_called_with('http://localhost:10080/find/random')
 
     def test_home_post_not_allowed(self):
         result = self.app.post(self.url)
-        assert STATUS_405 in result.status
+        self.assertEquals(STATUS_405, result.status)
+        #self.assertEquals(result.json, dict(success=True))
 
     def test_home_put_not_allowed(self):
         result = self.app.put(self.url)
-        assert STATUS_405 in result.status
+        self.assertEquals(STATUS_405, result.status)
 
     def test_home_delete_not_allowed(self):
         result = self.app.delete(self.url)
-        assert STATUS_405 in result.status
+        self.assertEquals(STATUS_405, result.status)
 
 
 class AdminUnitTests(unittest.TestCase):
@@ -51,8 +53,9 @@ class AdminUnitTests(unittest.TestCase):
     def test_admin_get(self, mocked):
         os.environ['BACKEND_URL'] = 'http://localhost:10080'
         result = self.app.get('/admin')
-        assert STATUS_200 in result.status
-        assert 'Ny krog' in result.data
+        self.assertEquals(STATUS_200, result.status)
+        self.assertTrue('class="logo shadow">Krogrouletten</div>' in result.data)
+        self.assertTrue('Ny krog' in result.data)
         mocked.get.assert_called_with('http://localhost:10080/find/all')
 
 
@@ -67,5 +70,6 @@ class PopupUnitTests(unittest.TestCase):
 
     def test_popup_get(self):
         result = self.app.get('/admin/popup')
-        assert STATUS_200 in result.status
-        assert 'Skicka' in result.data
+        self.assertEquals(STATUS_200, result.status)
+        self.assertFalse('class="logo shadow">Krogrouletten</div>' in result.data)
+        self.assertTrue('Skicka' in result.data)
