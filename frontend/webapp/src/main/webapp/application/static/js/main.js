956cc09ff54c9be34e863f3f1a20666d389cb2c7
David Eriksson <davideriksson@swedenmail.com>
2016-06-15 19:21:35 +0200

    Removed fancybox from js

--- frontend/webapp/src/main/webapp/application/static/js/main.js
+++ frontend/webapp/src/main/webapp/application/static/js/main.js
@@ -1,28 +1,6 @@
 
 $(document).ready(function () {
     'use strict';
-    $(".fancybox")
-        .attr('rel', 'gallery')
-        .fancybox({
-            maxWidth: 800,
-            maxHeight: 400,
-            fitToView: false,
-            width: '70%',
-            height: '70%',
-            autoSize: false,
-            padding: 0,
-            margin: [20, 60, 20, 60],
-            afterClose: function () {
-                location.reload();
-                return;
-            }
-        });
-
-    var closePopup, x;
-    closePopup = $("#closePopup").text();
-    if (closePopup !== "") {
-        parent.jQuery.fancybox.close();
-    }
 
     function showPosition(position) {
         $("#latitude").val(position.coords.latitude);
