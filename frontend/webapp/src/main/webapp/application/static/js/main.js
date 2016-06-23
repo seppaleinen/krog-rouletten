8d64290084463fa3b6ea29550f0ca6e2d245b001
David Eriksson <davideriksson@swedenmail.com>
2016-06-23 15:49:03 +0200

    Datalist on all addressfields

--- frontend/webapp/src/main/webapp/application/static/js/main.js
+++ frontend/webapp/src/main/webapp/application/static/js/main.js
@@ -1,5 +1,4 @@
-
-$(document).ready(function () {
+$(document).ready(function() {
     'use strict';
 
     function showPosition(position) {
@@ -7,13 +6,14 @@ $(document).ready(function () {
         $("#longitude").val(position.coords.longitude);
         $("#hidden_submit").click()
     }
-    function onError(error){
-        document.location= "/error";
+
+    function onError(error) {
+        document.location = "/error";
     }
     var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
     };
 
     function getLocation() {
@@ -24,33 +24,103 @@ $(document).ready(function () {
         }
     }
 
-    $("#slumpaGPS").click(function () {
-            getLocation();
-        }
-    );
+    function search_adress_datalist() {
+        var val = $(this).val();
+        if (val === "") return;
+        if (val.length < 10) return;
+        if (val === last_adress) return;
+        if (val.indexOf(' ') < 0) return;
+        var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(val) + "&sensor=false";
+        $.get(url, function(res) {
+            var dataList = $("#search_adress_list");
+            dataList.empty();
+            var len = res.results.length
+            if (res.results.length) {
+                for (var i = 0, len = res.results.length; i < len; i++) {
+                    var adress = res.results[i].formatted_address;
+                    last_adress = adress;
+                    var opt = $("<option></option>").attr("value", adress);
+                    dataList.append(opt);
+                }
+            }
+        }, "json");
+    };
 
+    function user_krog_adress_datalist() {
+        var val = $(this).val();
+        if (val === "") return;
+        if (val.length < 10) return;
+        if (val === last_adress) return;
+        if (val.indexOf(' ') < 0) return;
+        var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(val) + "&sensor=false";
+        $.get(url, function(res) {
+            var dataList = $("#user_krog_adress_list");
+            dataList.empty();
+            var len = res.results.length
+            if (res.results.length) {
+                for (var i = 0, len = res.results.length; i < len; i++) {
+                    var adress = res.results[i].formatted_address;
+                    last_adress = adress;
+                    var opt = $("<option></option>").attr("value", adress);
+                    dataList.append(opt);
+                }
+            }
+        }, "json");
+    };
+
+    function admin_ny_krog_adress_datalist() {
+        var val = $(this).val();
+        if (val === "") return;
+        if (val.length < 10) return;
+        if (val === last_adress) return;
+        if (val.indexOf(' ') < 0) return;
+        var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(val) + "&sensor=false";
+        $.get(url, function(res) {
+            var dataList = $("#admin_ny_krog_adress_list");
+            dataList.empty();
+            var len = res.results.length
+            if (res.results.length) {
+                for (var i = 0, len = res.results.length; i < len; i++) {
+                    var adress = res.results[i].formatted_address;
+                    last_adress = adress;
+                    var opt = $("<option></option>").attr("value", adress);
+                    dataList.append(opt);
+                }
+            }
+        }, "json");
+    };
+
+    function admin_update_krog_adress_datalist() {
+        var val = $(this).val();
+        if (val === "") return;
+        if (val.length < 10) return;
+        if (val === last_adress) return;
+        if (val.indexOf(' ') < 0) return;
+        var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(val) + "&sensor=false";
+        $.get(url, function(res) {
+            var dataList = $("#admin_update_krog_adress_list");
+            dataList.empty();
+            var len = res.results.length
+            if (res.results.length) {
+                for (var i = 0, len = res.results.length; i < len; i++) {
+                    var adress = res.results[i].formatted_address;
+                    last_adress = adress;
+                    var opt = $("<option></option>").attr("value", adress);
+                    dataList.append(opt);
+                }
+            }
+        }, "json");
+    };
+
+    $("#slumpaGPS").click(function() {
+        getLocation();
+    });
     var last_adress;
-	if(document.createElement("datalist").options) {
-        $('#sok_adress').on('input', function() {
-    		var val = $( this ).val();
-    		if(val === "") return;
-    		if(val.length < 10) return;
-    		if(val === last_adress) return;
-    		if(val.indexOf(' ') < 0) return;
-    		var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(val) + "&sensor=false";
-    		$.get(url, function(res) {
-    			var dataList = $("#adress_list");
-    			dataList.empty();
-    			var len=res.results.length
-    			if(res.results.length) {
-    				for(var i=0, len=res.results.length; i<len; i++) {
-    				    var adress = res.results[i].formatted_address;
-    				    last_adress = adress;
-    					var opt = $("<option></option>").attr("value", adress);
-    					dataList.append(opt);
-    				}
-    			}
-    		},"json");
-    	});
+
+    if (document.createElement("datalist").options) {
+        $('#sok_adress').on('input', search_adress_datalist);
+        $('#user_krog_adress').on('input', user_krog_adress_datalist);
+        $('#admin_ny_krog_adress').on('input', admin_ny_krog_adress_datalist);
+        $('#admin_update_krog_adress').on('input', admin_update_krog_adress_datalist);
     }
 });
