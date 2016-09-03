a0cd025c504132cd67fe7963b815c84248e23358
David Eriksson <davideriksson@swedenmail.com>
2016-09-03 22:44:27 +0200

    Pom structure

--- backend/src/main/java/se/doktorn/backend/KrogRoulettenApplication.java
+++ backend/src/main/java/se/doktorn/backend/KrogRoulettenApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories()
+@EnableMongoRepositories
 public class KrogRoulettenApplication {
     public static void main(String[] args) {
         SpringApplication.run(KrogRoulettenApplication.class, args);
