d1ed238138f46a93df1cca2d89f955ac821a283e
David Eriksson <davideriksson@swedenmail.com>
2016-09-16 19:03:25 +0200

    Sentry logging for flask webapp

--- frontend/webapp/src/main/webapp/application/__init__.py
+++ frontend/webapp/src/main/webapp/application/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from raven.contrib.flask import Sentry
 import sys
 app = Flask(__name__)
 app.debug=True
 if sys.version[0] == '2':
     reload(sys)
     sys.setdefaultencoding("utf-8")
+
+sentry = Sentry(app, dsn='http://d8cca3633d034a2e819e3fd21c40e018:b617d55e41af4735a80a11ef15b4a916@192.168.99.100:9000/2')
